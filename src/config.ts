c81c64240e8b1a540d24829e55d3ac5a5ce312f7
Flygeon <v2972632024@163.com>
2026-05-20 23:05:46 +0800

    上传文件

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Flygeon",
+	bio: "音无结弦之时，悦动天使之心；立于浮华之世，奏响天籁之音",
 	links: [
 		{
 			name: "Twitter",
